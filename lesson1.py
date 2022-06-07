5643bc265e026d12dd4808314f5717472b9e01d8
asce <190970161@qq.com>
2022-06-07 20:58:25 +0800

    lesson1 ver1

--- lesson1.py
+++ lesson1.py
@@ -1,4 +1,5 @@
 import keyword
+import sys
 '''
 1、第一個字符必須是字母或下劃綫
 2、標識符的其他部分由字母、數字和下劃綫組成
@@ -24,5 +25,13 @@ import keyword
 #字符串不能改變，字符串截取的語法格式 var[頭下標:尾下標:步長]
 str = '''123456789'''
 print(str[2:5]) #這個的意思是，從下標索引2開始，包括2，到索引6，但不包括6對應的數字。所以輸出是345
-print('hello\nworld')
-print(r'hello\nworld')
+# print('hello\nworld')
+# print(r'hello\nworld')
+
+#空行主要用于区分代码块，python并不会对空行进行处理
+
+#用户输入
+# input("enter your word:\n")
+
+sys.stdout.write("hi")
+sys.stdout.write(str)
