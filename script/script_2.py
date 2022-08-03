f6c0ef80651220300188909bd9a59aed3995ae37
asce <190970161@qq.com>
2022-08-03 16:05:51 +0800

    add new script

--- script/script_2.py
+++ script/script_2.py
@@ -10,16 +10,16 @@ argsparse会将传递的参数默认为字符串，可以通过加上参数type=
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-echo",help ="echo the argument you input")
-parser.add_argument("-s","-square",type=int,help="display a square of a given number")
+parser.add_argument("-s","--square",type=int,help="display a square of a given number")
 parser.add_argument("-v","--verbose",action="store_true",help="increase output verbosity")
 args = parser.parse_args()
 
 if args.verbose:
     print("verbosity turned on")
-if args.s:
-    print("Argument you inputed is:",str(args.s))
-    print("Square number is:",str(args.s**2))
+if args.square:
+    print("Argument you inputed is:",str(args.square))
+    print("Square number is:",str(args.square**2))
 else:
-    print("You did not input any number with -s")
+    print("You did not input any number with -s,please use --help")
 
 
